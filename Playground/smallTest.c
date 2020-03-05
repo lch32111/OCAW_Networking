f5874fdf10bbab9b241ca66f7a748b499792c3a4
lch32111 <lch32111@naver.com>
2020-03-06 01:05:33 +0900

    added Makefile for vim compile system

--- Playground/smallTest.c
+++ Playground/smallTest.c
@@ -2,6 +2,53 @@
 #include <inttypes.h>
 #include <arpa/inet.h>
 
+// Reference struct
+struct sockaddr_ref
+{
+	unsigned short 	sa_family;	// address family, AF_XXX
+	char		sa_data[14];	// 14 bytes of protocl address
+};
+
+struct addrinfo_ref
+{
+	int ai_flags;			// AI_PASSIVE, AI_CANONNAME, etc.
+	int ai_family;			// AF_INET, AF_INET6, AF_UNSPEC
+	int ai_socktype;		// SOCK_STREAM (TCP), SOCK_DGRAM (UDP)
+	int ai_protocol;		// use 0 for "any"
+	size_t ai_addrlen;		// size of ai_addr in bytes
+	struct sockaddr_ref* ai_addr;	// struct sockaddr_in or _in6
+	char* ai_canonname;		// full canonical hostname
+
+	struct addrinfo_ref* ai_next;	// linked list, next node
+};
+
+struct in_addr_ref
+{
+	uint32_t s_addr;	// that's a 32-bit int (4 bytes)
+};
+
+struct sockaddr_in_ref
+{
+	short int 		sin_family;	// Address family, AF_INET
+	unsigned short int 	sin_port;	// Port number
+	struct in_addr_ref	sin_addr;	// Internet address
+	unsigned char		sin_zero[8];	// Same size as struct sockaddr
+};
+
+struct in6_addr_ref
+{
+	unsigned char s_addr[16];	// IPv6 address
+};
+
+struct sockaddr_in6_ref
+{
+	u_int16_t 		sin6_family;	// address family, AF_INET6
+	u_int16_t 		sin6_port;	// port number, Network Byte Order
+	u_int32_t 		sin6_flowinfo;	// IPv6 flow information
+	struct in6_addr_ref 	sin6_addr;	// IPv6 address
+	u_int32_t		sin6_scope_id;	// Scope ID
+};
+
 int main(int argc, char* argv[])
 {
 	printf("Hello Networking World!\n");
